138942e64090ea3b21c335f2fd72e98ab896ab5e
Ampofua <adwoaampofua@gmail.com>
2024-08-15 22:01:26 +0000

    Week 2

---
+++ list.py
@@ -0,0 +1,23 @@
+# Q1
+my_list = []
+
+# Q2
+my_list.extend([10, 20, 30, 40])
+
+#Q3
+my_list.insert(1, 15)
+
+#Q4
+my_list.extend([50, 60, 70])
+
+#Q5
+my_list.pop(7)
+
+#Q6
+my_list.sort()
+
+#Q7
+print(my_list.index(30))
+
+
+
